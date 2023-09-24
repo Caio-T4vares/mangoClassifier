f7960d9d8546466107272771c29ee101135049dc
Caio <102376017+Caio-T4vares@users.noreply.github.com>
2023-09-24 19:28:52 -0300

    uiui

--- mangoClassifier.py
+++ mangoClassifier.py
@@ -1,7 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np 
-import os
-import cv2 as cv # para trabalhar com as imagens
 import tensorflow as tf
 
 from tensorflow import keras
@@ -9,7 +7,7 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 import pathlib
-data_dir = "mangos/MangoFruitDDS/SenMangoFruitDDS_original"
+data_dir = "mangoClassifier/mangos/MangoFruitDDS/SenMangoFruitDDS_original"
 data_dir = pathlib.Path(data_dir) # diretório que contem as imagens do dataset com background
 
 train_ds = tf.keras.utils.image_dataset_from_directory(
@@ -24,7 +22,7 @@ val_ds = tf.keras.utils.image_dataset_from_directory(
   data_dir,
   validation_split=0.2,
   subset="validation",
-  seed=123,
+  seed=223,
   image_size=(224, 224),
   batch_size=32)
 
@@ -38,23 +36,21 @@ val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 num_classes = len(class_names)
 data_augmentation = keras.Sequential(
   [
-    layers.RandomFlip("horizontal",
-                      input_shape=(224,
-                                  224,
-                                  3)),
+    layers.RandomFlip("horizontal",input_shape=(224,224,3)),
     layers.RandomRotation(0.1),
     layers.RandomZoom(0.1),
   ]
 )
 model = Sequential([
-  layers.Rescaling(1./255, input_shape=(224, 224, 3)),
   data_augmentation,
+  layers.Rescaling(1./255),
   layers.Conv2D(16, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(64, 3, padding='same', activation='relu'),
   layers.MaxPooling2D(),
+  layers.Dropout(0.2),
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
@@ -64,7 +60,7 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 model.summary()
-epochs=10
+epochs=20
 history = model.fit(
   train_ds,
   validation_data=val_ds,
@@ -92,3 +88,156 @@ plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 plt.show()
+
+# predict
+path = "mangoClassifier/mangateste.jpeg"
+img = tf.keras.utils.load_img(
+    path,
+    grayscale=False,
+    color_mode='rgb',
+    target_size=(224,224),
+    interpolation='nearest',
+    keep_aspect_ratio=False
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
+# segunda
+path = "mangoClassifier/antracnose.jpeg"
+img = tf.keras.utils.load_img(
+    path,
+    grayscale=False,
+    color_mode='rgb',
+    target_size=(224,224),
+    interpolation='nearest',
+    keep_aspect_ratio=False
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
+# terceira
+path = "mangoClassifier/mangaVerdeTeste.jpeg"
+img = tf.keras.utils.load_img(
+    path,
+    grayscale=False,
+    color_mode='rgb',
+    target_size=(224,224),
+    interpolation='nearest',
+    keep_aspect_ratio=False
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
+# quarta
+path = "mangoClassifier/antracnose2.jpg"
+img = tf.keras.utils.load_img(
+    path,
+    grayscale=False,
+    color_mode='rgb',
+    target_size=(224,224),
+    interpolation='nearest',
+    keep_aspect_ratio=False
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
+# quinta
+path = "mangoClassifier/stem_end_rot_3.jpg"
+img = tf.keras.utils.load_img(
+    path,
+    grayscale=False,
+    color_mode='rgb',
+    target_size=(224,224),
+    interpolation='nearest',
+    keep_aspect_ratio=False
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
+# sexta
+path = "mangoClassifier/stemEndRot.jpeg"
+img = tf.keras.utils.load_img(
+    path,
+    grayscale=False,
+    color_mode='rgb',
+    target_size=(224,224),
+    interpolation='nearest',
+    keep_aspect_ratio=False
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
+# setima
+path = "mangoClassifier/black_mould_rot_2.jpg"
+img = tf.keras.utils.load_img(
+    path,
+    grayscale=False,
+    color_mode='rgb',
+    target_size=(224,224),
+    interpolation='nearest',
+    keep_aspect_ratio=False
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
