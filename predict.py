122968aad6d7746460f0a283b63f22d7cc110b05
Caio Vinicius <caio.hosana@hotmail.com>
2023-09-24 21:46:20 -0300

    Little alterations m

--- predict.py
+++ predict.py
@@ -139,3 +139,25 @@ print(
     "This image most likely belongs to {} with a {:.2f} percent confidence."
     .format(class_names[np.argmax(score)], 100 * np.max(score))
 )
+
+# setima
+path = "mangoClassifier/stemEndRot.jpeg"
+img = tf.keras.utils.load_img(
+    path,
+    grayscale=False,
+    color_mode='rgb',
+    target_size=(224,224),
+    interpolation='nearest',
+    keep_aspect_ratio=False
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
