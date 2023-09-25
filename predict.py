d1ecdee591ad135158fc8fe38a8ca5fce7fe9f22
Caio <102376017+Caio-T4vares@users.noreply.github.com>
2023-09-24 21:32:48 -0300

    agora ism

---
+++ predict.py
@@ -0,0 +1,141 @@
+import matplotlib.pyplot as plt
+import numpy as np 
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+model = tf.keras.models.load_model('mangoClassifier/model/mangoModel')
+model.summary()
+class_names = ['Alternaria', 'Anthracnose', 'Black_Mould_Rot', 'Healthy', 'Stem_end_Rot']
+# predict
+path = "mangoClassifier/mangateste.jpeg"
+img = tf.keras.utils.load_img(
+    path,
+    grayscale=False,
+    color_mode='rgb',
+    target_size=(224,224),
+    interpolation='nearest',
+    keep_aspect_ratio=False
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
+# segunda
+path = "mangoClassifier/antracnose.jpeg"
+img = tf.keras.utils.load_img(
+    path,
+    grayscale=False,
+    color_mode='rgb',
+    target_size=(224,224),
+    interpolation='nearest',
+    keep_aspect_ratio=False
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
+# terceira
+path = "mangoClassifier/mangaVerdeTeste.jpeg"
+img = tf.keras.utils.load_img(
+    path,
+    grayscale=False,
+    color_mode='rgb',
+    target_size=(224,224),
+    interpolation='nearest',
+    keep_aspect_ratio=False
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
+# quarta
+path = "mangoClassifier/antracnose2.jpg"
+img = tf.keras.utils.load_img(
+    path,
+    grayscale=False,
+    color_mode='rgb',
+    target_size=(224,224),
+    interpolation='nearest',
+    keep_aspect_ratio=False
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+
+
+# sexta
+path = "mangoClassifier/black_mould_teste.jpg"
+img = tf.keras.utils.load_img(
+    path,
+    grayscale=False,
+    color_mode='rgb',
+    target_size=(224,224),
+    interpolation='nearest',
+    keep_aspect_ratio=False
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
+# setima
+path = "mangoClassifier/alternaria_teste.jpg"
+img = tf.keras.utils.load_img(
+    path,
+    grayscale=False,
+    color_mode='rgb',
+    target_size=(224,224),
+    interpolation='nearest',
+    keep_aspect_ratio=False
+)
+img_array = tf.keras.utils.img_to_array(img)
+img_array = tf.expand_dims(img_array, 0) # Create a batch
+
+
+predictions = model.predict(img_array)
+score = tf.nn.softmax(predictions[0])
+
+print(
+    "This image most likely belongs to {} with a {:.2f} percent confidence."
+    .format(class_names[np.argmax(score)], 100 * np.max(score))
+)
